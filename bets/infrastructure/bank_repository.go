9d82f6b50def071e6e281e9599029332558c0c61
dantunesd <diney.antunes@catho.com>
2021-03-16 18:14:44 -0300

    feat: 🎸 add repositories

---
+++ bets/infrastructure/bank_repository.go
@@ -0,0 +1,37 @@
+package infrastructure
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type BankRepository struct {
+	Client *mongo.Client
+}
+
+func (b *BankRepository) Create(content interface{}) error {
+	collection := b.Client.Database("my-bets").Collection("banks")
+
+	_, err := collection.InsertOne(context.TODO(), content)
+	return err
+}
+
+func (b *BankRepository) Get(id string, output interface{}) error {
+	collection := b.Client.Database("my-bets").Collection("banks")
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+
+	return collection.FindOne(context.TODO(), filter).Decode(output)
+}
+
+func (b *BankRepository) Update(id string, content interface{}) error {
+	collection := b.Client.Database("my-bets").Collection("banks")
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+
+	_, err := collection.UpdateOne(context.TODO(), filter, content)
+	return err
+}
---
+++ bets/infrastructure/bet_repository.go
@@ -0,0 +1,32 @@
+package infrastructure
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type BetRepository struct {
+	Client *mongo.Client
+}
+
+func (b *BetRepository) Create(content interface{}) error {
+	collection := b.Client.Database("my-bets").Collection("bets")
+
+	_, err := collection.InsertOne(context.TODO(), content)
+	return err
+}
+
+func (b *BetRepository) Get(id string, output interface{}) error {
+	collection := b.Client.Database("my-bets").Collection("bets")
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+
+	return collection.FindOne(context.TODO(), filter).Decode(&output)
+}
+
+func (b *BetRepository) Update(id string, content interface{}) error {
+	return nil
+}
